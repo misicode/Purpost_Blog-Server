f6a0d35d62879aa653142d69fe11f32b1c20315f
misicode <alee_1225@hotmail.com>
2024-06-14 00:05:50 -0500

    feat: add cors configuration

---
+++ gateway-service/src/main/java/com/misicode/purpost/gatewayservice/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.misicode.purpost.gatewayservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Value("${purpost.origin}")
+    private String purpostOrigin;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(purpostOrigin));
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsWebFilter(source);
+    }
+}
